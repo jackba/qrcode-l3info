29c9e2a1dcfaaf853ad9a44236c0f7e3d7c53f5e
cl.guillermin@gmail.com <cl.guillermin@gmail.com@22f098a9-8086-63ee-5b0d-9dc2cf2f2359>
2012-04-25 17:35:02 +0000

    Création de la classe polynome et des classes associées.
    La classe polynome est dérivée en deux types:
    - polynomes alpha du type :     alpha^exp.x^exp
    - polynomes entier du type:     coefficient.x^exp
    
    avec  alpha, coefficient, exp appartenant aux entiers naturels.
    
    Il est possible de transformer un polynome alpha en polynome entier et inversement (voir méthodes toXXXXXPolynome() présentes dans chacune des classes).
    
    --------------------------------------------
    -- EXEMPLE de polynome --
    
    -- (CODE) --
    
    AlphaPolynom ap = new AlphaPolynom();
    ap.addNewTerme(0, 1);
    ap.addNewTerme(1, 1);
    System.out.println(ap);
    System.out.println(ap.toIntegerPolynom());
    
    -- (SORTIE) --
    
    a^0.x^1 + a^1.x^1
    1.x^1 + 2.x^1
    -------------------------------------------
    
    Cette transformation est réalisée grâce aux valeurs des logs/alogs générés par la classe GaloisField et qui correspondent aux valeur d'un champs de Galois de taille 256 et de polynome premier 285 (x^8 + x^4 + x^3 + 1).
    
    La classe GaloisField est désormais gérée en Singleton pour en assurer l'unicité (pas besoin d'avoir plusieurs fois la classe car elle génère deux tableaux lors d'une nouvelle instanciation, lesquels pourraient prendre une taille conséquente et ralentir le programme inutilement).

---
+++ trunk/src/PolynomialPack/IntegerPolynom.java
@@ -0,0 +1,29 @@
+package PolynomialPack;
+
+public class IntegerPolynom extends Polynom {
+	
+	public IntegerPolynom()
+	{
+		super();
+	}
+	
+	public void addNewTerme(int coefficient, int xExponent)
+	{
+		this.addNewTerme(new TermeEntier(coefficient, xExponent));
+	}
+	
+	// Retourne la version alpha de ce polynome entier
+	public AlphaPolynom toAlphaPolynom()
+	{
+		AlphaPolynom ap = new AlphaPolynom();
+		TermeEntier te;
+		
+		for (int i=0; i<this.getNbTermes(); i++)
+		{
+			te = (TermeEntier)getTermeAt(i);
+			ap.addNewTerme(te.toTermeAlpha());
+		}
+		
+		return ap;
+	}
+}
