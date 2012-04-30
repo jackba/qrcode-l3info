860c3134926c4e5dd501d2393288c6c890896f7c
cl.guillermin@gmail.com <cl.guillermin@gmail.com@22f098a9-8086-63ee-5b0d-9dc2cf2f2359>
2012-04-30 15:20:20 +0000

    Ajout des patterns de timing (les 2 lignes d'une case noire et blanche en alternance situées entre les carrés de positionnement)

--- trunk/src/Model/QRcode.java
+++ trunk/src/Model/QRcode.java
@@ -1,34 +1,55 @@
 package Model;
 
 public class QRcode {
-	
+
 	private int m_version;
 	private String m_binaryString;
 	private String m_correcLevel;
 	private Boolean[][] m_matrice;
 	private int m_matriceSize;
-	
+
 	public static void main(String[] args) {
 		QRcode code = new QRcode(1,"H","01010210");
 		code.fillQRmatrix();
 		System.out.println(code);
 	}
-	
+
 	public QRcode(int version, String correcLevel , String binaryDataEncoded)
 	{
 		m_binaryString = binaryDataEncoded;
 		m_correcLevel = correcLevel;
 		setVersion(version);
 	}
-	
+
 	// Remplit la matrice du QRcode en se basant sur la version, le niveau de correction et la chaine binaire
 	// False = carré blanc
 	// True = carré noir
 	public void fillQRmatrix()
 	{
 		fillFinderPatterns();
+		fillTimingPatterns();
 	}
-	
+
+	// Ajoute la ligne et la colonne composés d'une alternance de modules noirs et blanc
+	// et situés entre les Finders pattern (les 3 gros carrés de détection de position).
+	private void fillTimingPatterns()
+	{
+		// on commence toujours par un module noir et on termine toujours par un module noir
+		for (int col=8; col<m_matriceSize-8; col++)
+		{
+			if (col%2 == 0)	// Le nombre est pair: la case sera noire
+			{
+				m_matrice[6][col] = true;	// Patron horizontal
+				m_matrice[col][6] = true;	// Patron vertical
+			}
+			else	// Le nombre est impair: la case sera blanche
+			{
+				m_matrice[6][col] = false;	// Patron horizontal
+				m_matrice[col][6] = false;	// Patron vertical
+			}
+		}
+	}
+
 	// Ajoute les 3 patrons carrés de positionnement situés en haut à gauche de la matrice,
 	// en haut à droite de la matrice et en bas à gauche de la matrice
 	// Leur taille est fixée dans la spécification du QRcode et identique pour toutes les versions
@@ -37,39 +58,39 @@ public class QRcode {
 		int firstBlackSquare = 7;
 		int secondWhiteSquare = 5;
 		int thirdBlackSquare = 3;
-		
+
 		// Premier patron
 		for (int line=0; line<firstBlackSquare; line ++)
 			for (int col=0; col<firstBlackSquare; col++)
 				m_matrice[line][col] = true;
-		
+
 		for (int line=1; line<secondWhiteSquare+1; line ++)
 			for (int col=1; col<secondWhiteSquare+1; col++)
 				m_matrice[line][col] = false;
-		
+
 		for (int line=2; line<thirdBlackSquare+2; line ++)
 			for (int col=2; col<thirdBlackSquare+2; col++)
 				m_matrice[line][col] = true;
-		
+
 		for (int i=0; i<=firstBlackSquare; i++)
 		{
 			m_matrice[firstBlackSquare][i] = false;	// Remplissage ligne blanche
 			m_matrice[i][firstBlackSquare] = false;	// Remplissage colonne blanche
 		}
-		
+
 		// Second patron
 		for (int line=0; line<firstBlackSquare; line ++)
 			for (int col=m_matriceSize-firstBlackSquare; col<m_matriceSize; col++)
 				m_matrice[line][col] = true;
-		
+
 		for (int line=1; line<secondWhiteSquare+1; line ++)
 			for (int col=m_matriceSize-secondWhiteSquare-1; col<m_matriceSize-1; col++)
 				m_matrice[line][col] = false;
-		
+
 		for (int line=2; line<thirdBlackSquare+2; line ++)
 			for (int col=m_matriceSize-thirdBlackSquare-2; col<m_matriceSize-2; col++)
 				m_matrice[line][col] = true;
-		
+
 		/*for (int i=m_matriceSize-firstBlackSquare-1; i<m_matriceSize; i++)
 		{
 			m_matrice[firstBlackSquare][i] = false;	// Remplissage ligne blanche
@@ -80,27 +101,27 @@ public class QRcode {
 			m_matrice[firstBlackSquare][m_matriceSize-1-i] = false;	// Remplissage ligne blanche
 			m_matrice[i][m_matriceSize-1-firstBlackSquare] = false;	// Remplissage colonne blanche
 		}
-		
+
 		// Troisième patron
 		for (int line=m_matriceSize-firstBlackSquare; line<m_matriceSize; line ++)
 			for (int col=0; col<firstBlackSquare; col++)
 				m_matrice[line][col] = true;
-		
+
 		for (int line=m_matriceSize-secondWhiteSquare-1; line<m_matriceSize-1; line ++)
 			for (int col=1; col<secondWhiteSquare+1; col++)
 				m_matrice[line][col] = false;
-		
+
 		for (int line=m_matriceSize-thirdBlackSquare-2; line<m_matriceSize-2; line ++)
 			for (int col=2; col<thirdBlackSquare+2; col++)
 				m_matrice[line][col] = true;
-		
+
 		for (int i=0; i<=firstBlackSquare; i++)
 		{
 			m_matrice[m_matriceSize-firstBlackSquare-1][i] = false;	// Remplissage ligne blanche
 			m_matrice[m_matriceSize-1-i][firstBlackSquare] = false;	// Remplissage colonne blanche
 		}
 	}
-	
+
 	// Retourne le nombre de modules nécessaires pour un côté
 	// de la version de QRcode passée en paramètre
 	private int getModuleCount(int version)
@@ -110,13 +131,13 @@ public class QRcode {
 		// ce qui donne: module 2 = 25; module 3 = 29; ...
 		return 21 + 4*(version-1);
 	}
-	
+
 	// Retourne la matrice actuelle du QRcode
 	public Boolean[][] getQRmatrix()
 	{
 		return m_matrice;
 	}
-	
+
 	public String toString()
 	{
 		String result = "";
