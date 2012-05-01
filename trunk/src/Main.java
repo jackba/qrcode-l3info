8348e8c95bb73abaec81d53c9f68de8bbb82a811
cl.guillermin@gmail.com <cl.guillermin@gmail.com@22f098a9-8086-63ee-5b0d-9dc2cf2f2359>
2012-05-01 18:55:07 +0000

    suppression de l'opération de test placée dans le main

--- trunk/src/Main.java
+++ trunk/src/Main.java
@@ -5,8 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		MainController controller = new MainController();
 		controller.runApplication();
-		int r = 1 ^ 0;
-		System.out.println(r);
 	}
 	
 }
