6fae4b3282a0617931036e0f69371fc1c9745373
cl.guillermin@gmail.com <cl.guillermin@gmail.com@22f098a9-8086-63ee-5b0d-9dc2cf2f2359>
2012-05-02 16:59:16 +0000

    Possibilité d'afficher le QRcode généré dans le panel. Pour l'instant le QRcode généré est codé en dur dans l'actionPerformed du controller BgenererController

--- trunk/src/Controller/BgenererController.java
+++ trunk/src/Controller/BgenererController.java
@@ -3,12 +3,18 @@ package Controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import Model.BinaryStringGenerator;
+import Model.QRcode;
 import Vue.Fenetre;
 
 public class BgenererController extends AbstractController implements ActionListener {
 
+	private BinaryStringGenerator m_stringGenerator;
+	private QRcode m_qrCode;
+	
 	public BgenererController(Fenetre f) {
 		super(f);
+		m_stringGenerator = new BinaryStringGenerator();
 	}
 	
 	public boolean isValid() {
@@ -18,7 +24,10 @@ public class BgenererController extends AbstractController implements ActionList
 	// OnClick
 	public void actionPerformed(ActionEvent event)
 	{
-		
+		String encodedData = m_stringGenerator.getBinaryString("HELLO WORLD", 1, 7, "Q");
+		m_qrCode = new QRcode(7,"Q",encodedData);
+		m_qrCode.fillQRmatrix();
+		getFenetre().getQrPanel().drawImageFromMatrix(m_qrCode.getQRmatrix());
 	}
 
 }
