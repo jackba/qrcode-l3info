cbfe7a760a5ddd02a5387aeb8e7b4a39a16601ea
cl.guillermin@gmail.com <cl.guillermin@gmail.com@22f098a9-8086-63ee-5b0d-9dc2cf2f2359>
2012-05-10 14:45:12 +0000

    Ajout du décodage d'un qr code image

---
+++ trunk/src/Vue/ImageComponent.java
@@ -0,0 +1,54 @@
+package Vue;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+// Panel affichant simplement une image
+@SuppressWarnings("serial")
+public class ImageComponent extends JPanel {
+	
+	private Image m_image;
+	
+	public ImageComponent(){}
+	
+	public ImageComponent(Image img)
+	{
+		m_image = img;
+		refreshImage();
+	}
+	
+	protected void paintComponent(Graphics g) {
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		if (m_image != null)
+		g.drawImage(m_image, 0, 0, this.getWidth(), this.getHeight(), this);
+	}
+	
+	public void setImage(Image img)
+	{
+		m_image = img;
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				refreshImage();
+			}
+		});
+	}
+	
+	public void refreshImage()
+	{
+		if (m_image != null)
+		{
+			this.setPreferredSize(new Dimension(m_image.getWidth(null), m_image.getHeight(null)));
+			this.setMinimumSize(this.getPreferredSize());
+			this.setMaximumSize(this.getPreferredSize());
+			this.setSize(this.getPreferredSize());
+			this.repaint();
+			this.updateUI();
+		}
+	}
+}
